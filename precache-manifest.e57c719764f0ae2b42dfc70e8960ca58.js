258b0a7e6f5fd6868aa68d6b9c72664fa5f4ddf8
karyasov <karyasov@haulmont.com>
2020-09-26 12:50:13 +0400

    Updates

---
+++ precache-manifest.e57c719764f0ae2b42dfc70e8960ca58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bdc0e9e5fa45826c719d11975bb6b6",
+    "url": "/react-router-app/index.html"
+  },
+  {
+    "revision": "a60d744b4ac74caa90af",
+    "url": "/react-router-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97d66997560c9e5c4386",
+    "url": "/react-router-app/static/js/2.86b84eb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-router-app/static/js/2.86b84eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60d744b4ac74caa90af",
+    "url": "/react-router-app/static/js/main.daa6626c.chunk.js"
+  },
+  {
+    "revision": "2d551724ed1974425060",
+    "url": "/react-router-app/static/js/runtime-main.64ddaf04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-router-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
