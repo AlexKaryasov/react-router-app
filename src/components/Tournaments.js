1c840549a1255278d70f55a9a20cf32129650b1a
karyasov <karyasov@haulmont.com>
2020-09-26 13:24:19 +0400

    Added dummy navigation

---
+++ src/components/Tournaments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tournaments = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Tournaments</h4>
+        </div>
+    )
+}
+
+export default Tournaments
