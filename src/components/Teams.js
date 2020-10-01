06880e5625c236b2d670274fa2786634518dd4a7
karyasov <karyasov@haulmont.com>
2020-10-01 15:12:57 +0400

    added redux to our project

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class Teams extends Component {
-    state = {
-        teams: []
-    }
     render() {
-        const { teams } = this.state
+        const { teams } = this.props
         const teamList = teams.length ? (
             teams.map(team => {
                 return (
                     <div className="team card" key={team.id}>
                         <div className="card-content">
                             <Link to={'/' + team.id}><span className="card-title">{team.title}</span></Link>
-                            <p>{team.body}</p>
+                            <p>{team.rankings}</p>
                         </div>
                     </div>
                 )
@@ -30,17 +27,12 @@ class Teams extends Component {
             </div>
         )
     }
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                console.log(response.data)
-                this.setState({
-                    teams: response.data.slice(0, 10)
-                })
-            })
-    }
-
 }
 
-export default Teams
+const mapStateToProps = (state) => {
+    return {
+        teams: state.teams
+    }
+}
+
+export default connect(mapStateToProps)(Teams)
