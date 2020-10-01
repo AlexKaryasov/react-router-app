86e8082d19e4f9da5b10e8a798202edb027edf39
karyasov <karyasov@haulmont.com>
2020-10-01 21:55:13 +0400

    added redux event dispatch

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,22 @@
 const initState = {
     teams: [
-        {id: 59580, title: 'И остались голодными', rankings: 59},
-        {id: 185, title: 'Гимназисты', rankings: 53},
-        {id: 670, title: 'Ксеп', rankings: 33},
+        {id: '59580', title: 'И остались голодными', rankings: 59},
+        {id: '185', title: 'Гимназисты', rankings: 53},
+        {id: '670', title: 'Ксеп', rankings: 33},
     ]
 }
 
 const rootReducer = (state = initState, action) => {
+    if (action.type === "DELETE_TEAM") {
+        let newTeams = state.teams.filter((team) => {
+            return team.id !== action.id
+        })
+
+        return {
+            ...state,
+            teams: newTeams
+        }
+    }
     return state;
 }
 
