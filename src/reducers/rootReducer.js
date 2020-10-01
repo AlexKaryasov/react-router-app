d3f32a8d4a478beb483c0089eb426369ca17a03b
karyasov <karyasov@haulmont.com>
2020-10-01 15:12:57 +0400

    added redux to our project

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    teams: [
+        {id: 59580, title: 'И остались голодными', rankings: 59},
+        {id: 185, title: 'Гимназисты', rankings: 53},
+        {id: 670, title: 'Ксеп', rankings: 33},
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer;
